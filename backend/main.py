2c05b29ad8455b08b41c23bacb187bd2b6a553e0
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-03-08 17:28:33 +0530

    enabled cors for frontend backend communication

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routers import papers, chat
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="ResearchHub AI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(papers.router)
 app.include_router(chat.router)
